62be8cea2739dedce5328a47fd0f9de0926261e3
n00bg0j <ryan_lee_94@hotmail.com>
2016-07-14 02:27:00 -0400

    Added starter code.

---
+++ GaussInteger.java
@@ -0,0 +1,50 @@
+package e1;
+
+public class GaussInteger {
+	private int real, imag;
+	
+	GaussInteger() {
+		this.real = 0;
+		this.imag = 0;
+	}
+	
+	GaussInteger(int real, int imag) {
+		// your code goes here
+	}
+	
+	public int getReal() {
+		return real;
+	}
+
+	public void setReal(int real) {
+		this.real = real;
+	}
+
+	public int getImag() {
+		return imag;
+	}
+
+	public void setImag(int imag) {
+		this.imag = imag;
+	}
+
+	// define and code the add method below
+	
+	// define and code the moduleSquared method below
+	
+	// define and code the isInvertible method below
+	
+	// Add the necessary formal parameter
+	public boolean equals() {
+	   boolean result = true;
+	   // write code that assigns the correct value to result
+	   return result;
+	}
+	
+	public String toString() {
+		String result = "";
+		// your code goes here
+		return result;
+	}
+
+}
